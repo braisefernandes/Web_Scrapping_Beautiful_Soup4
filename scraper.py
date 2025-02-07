a68f9a9478bc09aaa938fcbc4ba20f60481a23b3
braisefernandes <braisefernandes@gmail.com>
2025-02-07 15:00:42 +0530

    Initial Commit

--- scraper.py
+++ scraper.py
@@ -19,25 +19,25 @@ with open("index.html", "w+") as f:
     f.write(x)
 
 
-# courses = []
-# for course in soup.find_all('a', class_='course-card-title'): 
-#     title = course.get_text(strip=True)
-#     link = course.get('href')
-#     if title and link:
-#         courses.append([title, link])
+courses = []
+for course in soup.find_all('a', class_='course-card-title'): 
+    title = course.get_text(strip=True)
+    link = course.get('href')
+    if title and link:
+        courses.append([title, link])
 
-# languages = []
-# for language in soup.find_all('a', class_='language-card-title'):  
-#     title = language.get_text(strip=True)
-#     link = language.get('href')
-#     if title and link:
-#         languages.append([title, link])
+languages = []
+for language in soup.find_all('a', class_='language-card-title'):  
+    title = language.get_text(strip=True)
+    link = language.get('href')
+    if title and link:
+        languages.append([title, link])
 
-# # Save data to a CSV file
-# with open("geeks_courses_languages_1.csv", "w", newline="", encoding="utf-8") as file:
-#     writer = csv.writer(file)
-#     writer.writerow(["Title", "Link"])
-#     writer.writerows(courses)
-#     writer.writerows(languages)
+# Save data to a CSV file
+with open("geeks_courses_languages_1.csv", "w", newline="", encoding="utf-8") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Title", "Link"])
+    writer.writerows(courses)
+    writer.writerows(languages)
 
-# print("CSV file 'geeks_courses_languages.csv' has been created successfully!")
+print("CSV file 'geeks_courses_languages.csv' has been created successfully!")
